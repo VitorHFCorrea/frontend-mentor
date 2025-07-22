fbdc6845cab312ab06f6d1ad0101899ee0473116
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-07-22 01:07:02 -0300

    Create script.js

---
+++ Challenges/Newbie/interactive-rating-component/src/assets/js/script.js
@@ -0,0 +1,13 @@
+const submitBtn = document.getElementById('submit-btn');
+const ratingBtn = document.querySelectorAll('.rating-btn');
+
+ratingBtn.addEventListener('click', () => {
+  const selectedBtn = document.
+  ratingBtn.classList.add('')
+})
+
+submitBtn.addEventListener('click', () => {
+
+  const selectedBtn = document.getElementsByClassName
+})
+
