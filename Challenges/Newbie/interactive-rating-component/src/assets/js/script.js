e13bdd41a6a4f78b06c09f70739f7732fee55305
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-07-22 12:23:26 -0300

    added actual functions to enhance the code

--- Challenges/Newbie/interactive-rating-component/src/assets/js/script.js
+++ Challenges/Newbie/interactive-rating-component/src/assets/js/script.js
@@ -1,32 +1,38 @@
-const submitBtn = document.getElementById('submit-btn');
-const ratingBtns = document.querySelectorAll('.rating-btn');
-const feedbackContainer = document.getElementById('feedback-container');
-const thanksContainer = document.getElementById('thanks-container');
-const rateDisplay = document.getElementById('rate');
+const submitBtn = document.getElementById('submit-btn')
+const ratingBtns = document.querySelectorAll('.rating-btn')
+const feedbackContainer = document.getElementById('feedback-container')
+const thanksContainer = document.getElementById('thanks-container')
+const rateDisplay = document.getElementById('rate')
 
-let selectedRating = null;
+let selectedRating = null
 
-ratingBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    ratingBtns.forEach(btn => btn.classList.remove("selected"));
-    btn.classList.add("selected");
-    selectedRating = btn.textContent;
-  })
-});
+function ratingClick(event) {
+  selectRating(event.target)
+}
 
-submitBtn.addEventListener('click', (event) => {
+function selectRating(btn) {
+  ratingBtns.forEach((btn) => btn.classList.remove('selected'))
+  btn.classList.add('selected')
+  selectedRating = btn.textContent
+}
+
+function submitClick(event) {
   event.preventDefault()
+  if (selectedRating) {
+    showThanksScreen()
+  } else {
+    alert('Please select a rating before submitting.')
+  }
+}
 
-  if (selectedRating){
-    feedbackContainer.classList.add('hidden');
-    thanksContainer.classList.remove('hidden');
-    rateDisplay.textContent = selectedRating;
-  }
-  else {
-    alert('Please select a rating before submitting.');
-  }
+function showThanksScreen() {
+  feedbackContainer.classList.add('hidden')
+  thanksContainer.classList.remove('hidden')
+  rateDisplay.textContent = selectedRating
+}
+
+ratingBtns.forEach((btn) => {
+  btn.addEventListener('click', ratingClick)
 })
 
-
-
-
+submitBtn.addEventListener('click', submitClick)
