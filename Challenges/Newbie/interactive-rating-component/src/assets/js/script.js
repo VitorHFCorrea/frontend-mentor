bba99ddfcab0fb1b09fa9392e014f1b0545d8046
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-07-22 12:05:14 -0300

    Added button functions

--- Challenges/Newbie/interactive-rating-component/src/assets/js/script.js
+++ Challenges/Newbie/interactive-rating-component/src/assets/js/script.js
@@ -1,13 +1,32 @@
 const submitBtn = document.getElementById('submit-btn');
-const ratingBtn = document.querySelectorAll('.rating-btn');
+const ratingBtns = document.querySelectorAll('.rating-btn');
+const feedbackContainer = document.getElementById('feedback-container');
+const thanksContainer = document.getElementById('thanks-container');
+const rateDisplay = document.getElementById('rate');
 
-ratingBtn.addEventListener('click', () => {
-  const selectedBtn = document.
-  ratingBtn.classList.add('')
+let selectedRating = null;
+
+ratingBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    ratingBtns.forEach(btn => btn.classList.remove("selected"));
+    btn.classList.add("selected");
+    selectedRating = btn.textContent;
+  })
+});
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  if (selectedRating){
+    feedbackContainer.classList.add('hidden');
+    thanksContainer.classList.remove('hidden');
+    rateDisplay.textContent = selectedRating;
+  }
+  else {
+    alert('Please select a rating before submitting.');
+  }
 })
 
-submitBtn.addEventListener('click', () => {
 
-  const selectedBtn = document.getElementsByClassName
-})
+
 
